3bf80d9f1cff2a056381c13d1f804cc93cff90fd
diksha <dikshatambe77@gmail.com>
2020-05-06 09:08:31 +0530

    Database schema

--- Acl/rest-api/resource/sql/001_create_table.sql
+++ Acl/rest-api/resource/sql/001_create_table.sql
@@ -1,14 +1,54 @@
-USE restapi;
+CREATE DATABASE IF NOT EXIST acl;
+drop database acl;
+create database acl;
+use acl;
 
-CREATE TABLE IF NOT EXISTS user_detail (
-    id                  INT         AUTO_INCREMENT      PRIMARY KEY,
-    first_name          CHAR(25)    NOT NULL,
-    last_name           CHAR(25)    NOT NULL,
-    email               CHAR(64)    NOT NULL UNIQUE,
-    password            VARBINARY(128)    NOT NULL,
-    contact_number      CHAR(15)    NOT NULL,
-    updated_by          INT         NOT NULL DEFAULT 0,
-    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
-    creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
-    last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)ENGINE = INNODB CHARACTER SET=utf8;
+DROP TABLE  IF NOT EXIST Users;
+DROP TABLE  IF NOT EXIST Groups;
+DROP TABLE  IF NOT EXIST Files;
+DROP TABLE  IF NOT EXIST UserGroup;
+DROP TABLE  IF NOT EXIST Permission;
+
+CREATE TABLE Groups (
+    GroupId int NOT NULL,
+    GroupName varchar(10) NOT NULL,
+    Owner varchar(20),
+    password varchar(10),
+    PRIMARY KEY (GroupId),
+);
+
+CREATE TABLE Permission (
+    PId int NOT NULL,
+    PName varchar NOT NULL,
+);
+
+CREATE TABLE Users (
+    UserId int NOT NULL,
+    userName varchar(10) NOT NULL,
+    password varchar(10),
+    FileId  INT,
+    PId  INT,
+    PRIMARY KEY (UserId),
+    FOREIGN KEY (FileId) REFERENCES File(FileId),
+    FOREIGN KEY (PId) REFERENCES Permission(PId)
+);
+
+CREATE TABLE Files (
+    FileId int NOT NULL,
+    FileName varchar(10) NOT NULL,
+    Owner varchar(20),
+    FOREIGN KEY (GroupId) REFERENCES Groups(GroupId),
+    FOREIGN KEY (FileId) REFERENCES File(FileId),
+    FOREIGN KEY (RoleId) REFERENCES Permission(PId)
+    PRIMARY KEY (FileId),
+);
+
+CREATE TABLE IF NOT EXISTS UserGroup (
+        UserId          INT,
+        GroupId         INT,
+        PRIMARY KEY (UserId, GroupId),
+        FOREIGN KEY (UserId) REFERENCES Users(UserId),
+        FOREIGN KEY (GroupId) REFERENCES Groups(GroupId)
+);
+
+
